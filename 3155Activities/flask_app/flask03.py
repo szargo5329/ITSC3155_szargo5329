42675201ce59c8d3ad98b0825b99fd5c2500762e
Stephen <szargo@uncc.edu>
2021-11-11 12:15:53 -0500

    flask app #3 added new note route and view

--- 3155Activities/flask_app/flask03.py
+++ 3155Activities/flask_app/flask03.py
@@ -7,13 +7,14 @@ from flask import render_template
 
 
 app = Flask(__name__)     # create an app
+stephenUser = {'name': 'Stephen', 'email':'szargo@uncc.edu'}
 
 # @app.route is a decorator. It gives the function "index" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
 # get called. What it returns is what is shown as the web page
 @app.route('/index')
 def index():
-    stephenUser = {'name': 'Mogli', 'email':'mogli@uncc.edu'}
+
     return render_template("index.html" , user = stephenUser)
 
 @app.route('/notes')
@@ -33,6 +34,10 @@ def get_note(note_id):
         3: {'title':'Third note', 'text':'This is my third note', 'date':'10-3-2020'}
     }
     return render_template('note.html', note = notes[int(note_id)])
+
+@app.route('/notes/new')
+def new_note():
+    return render_template('new.html', user = stephenUser)
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
