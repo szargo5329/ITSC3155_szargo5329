202ce7c92b3efdf6f45a456d8fb454a40952992a
Stephen <szargo@uncc.edu>
2021-12-02 11:41:42 -0500

    flask app #4 - added db connection to app

--- 3155Activities/flask_app/database.py
+++ 3155Activities/flask_app/database.py
@@ -1,19 +1,4 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from database import db
 
-# Initialize the Flask-SQLAlchemy extension instance
+# Initialize Flask-SQLAlchemy extension instance
 db = SQLAlchemy()
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
-
-#  Bind SQLAlchemy db object to this Flask app
-db.init_app(app)
-
-# Setup models
-with app.app_context():
-    db.create_all() # run under the app context
-
-
-
--- 3155Activities/flask_app/flask04.py
+++ 3155Activities/flask_app/flask04.py
@@ -12,27 +12,50 @@ from models import User as User
 from database import db
 
 
-app = Flask(__name__)     # create an app
 
-stephenUser = db.session.query(User).filter_by(email = 'szargo@uncc.edu')   # Get user object from the database
-notes = db.session.query(Note).all()    # Get note object from database
+app = Flask(__name__)   # initialize Flask app
+
+#Configure the database connection by setting the name and
+# location of the database file.
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
+
+# disable an unneeded feature for us of Flask-SQLAlchemy that signals the
+# application every time a change is about to be made in the database
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+
+#  Bind SQLAlchemy db object to this Flask app
+db.init_app(app)
+
+# Setup models
+with app.app_context():
+    db.create_all()   # run under the app context
+
+#stephenUser = db.session.query(User).filter_by(email = 'szargo@uncc.edu')   # Get user object from the database
+#notes = db.session.query(Note).all()    # Get note object from database
 
 # @app.route is a decorator. It gives the function "index" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
 # get called. What it returns is what is shown as the web page
 @app.route('/index')    # Landing page
 def index():
-
+    # get user from database
+    stephenUser =  db.session.query(User).filter_by(email='szargo@uncc.edu')
     return render_template("index.html" , user = stephenUser)
 
 @app.route('/notes')    # View page with all notes
 def get_notes():
-
-    return render_template('notes.html', notes = notes, user = stephenUser)
+    # retrieve user from database
+    stephenUser = db.session.query(User).filter_by(email='szargo@uncc.edu')
+    # retrieve notes from database
+    my_notes = db.session.query(Note).all()
+    return render_template('notes.html', notes = my_notes, user = stephenUser)
 
 @app.route('/note/<note_id>')   # View individual note
 def get_note(note_id):
-    my_note = db.session.query(Note).filter_by(id = note_id)
+    # retrieve user from database
+    stephenUser = db.session.query(User).filter_by(email='szargo@uncc.edu')
+    # retrieve note from database
+    my_note = db.session.query(Note).filter_by(id=note_id)
     return render_template('note.html', note = my_note, user = stephenUser)
 
 @app.route('/notes/new', methods = ['GET', 'POST']) # Post a new note
@@ -60,7 +83,7 @@ def new_note():
     else:
         # GET request - show new note page
         # retrieve user from database
-        user = db.session.query(User).filter_by(email = 'szargo@uncc.edu')
+        stephenUser = db.session.query(User).filter_by(email = 'szargo@uncc.edu')
         return render_template('new.html', user = stephenUser)
 
 
